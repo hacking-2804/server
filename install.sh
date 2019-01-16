76221456f556a673ad061ef286adda0cbccbcea8
Forest Anderson <forestkzanderson@gmail.com>
2019-01-16 13:59:38 -0500

    Added force yes

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 sudo -i
 
-sudo apt-get install tmux git
+sudo apt-get update
+sudo apt-get install tmux git -y
 
 # install docker
-sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg2 \
-    software-properties-common
+    software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce -y
 sudo groupadd docker
 sudo usermod -aG docker $usermod
 sudo service docker start
