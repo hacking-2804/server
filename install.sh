6282347ad87901e5f1430730f21aa5d83e7e7bf1
Forest Anderson <forestkzanderson@gmail.com>
2019-01-18 09:59:18 -0500

    Added Docker Compose

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-sudo apt-get update
-sudo apt-get install tmux git -y
-
 # Install Docker
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -9,14 +10,18 @@ sudo apt-get install \
     gnupg2 \
     software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
 sudo apt-get install docker-ce -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker start
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Install other software
+sudo apt-get install tmux git -y
 
 # Clone the repos
 git clone https://github.com/hacking-2804/accessible-course-material.git
